9882e65fb76861bba06d66a187fef13f5a19fb77
sfc9982 <26681591+sfc9982@users.noreply.github.com>
2022-11-10 15:32:21 +0800

    Create 7043.cpp

---
+++ 10/Exp.5-6/7043.cpp
@@ -0,0 +1,21 @@
+//
+// Created by sfc9982 on 2022/11/10.
+//
+
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <functional>
+
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
